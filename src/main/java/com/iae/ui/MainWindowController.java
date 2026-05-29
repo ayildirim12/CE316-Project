1a41d43165eff9b76471c1d747585797b44c1921
aysenazgelen <aysenazgelen@gmail.com>
2026-05-30 00:00:27 +0300

    conf import export
    MainWindowController.java needs testing

--- src/main/java/com/iae/ui/MainWindowController.java
+++ src/main/java/com/iae/ui/MainWindowController.java
@@ -1,15 +1,18 @@
 package com.iae.ui;
 
-import com.iae.logic.Compiler;
-import com.iae.logic.DefaultExecutor;
-import com.iae.logic.DefaultCompiler;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
 import com.iae.logic.ConfigurationManager;
 import com.iae.logic.DatabaseManager;
+import com.iae.logic.DefaultCompiler;
+import com.iae.logic.DefaultExecutor;
 import com.iae.logic.DirectoryScanner;
 import com.iae.logic.EvaluationEngine;
 import com.iae.logic.OutputComparator;
-import com.iae.logic.ProjectManager;
 import com.iae.logic.PipelineExecutor;
+import com.iae.logic.ProjectManager;
 import com.iae.model.ComparisonResult;
 import com.iae.model.Configuration;
 import com.iae.model.EvaluationResult;
@@ -17,6 +20,7 @@ import com.iae.model.ExecutionResult;
 import com.iae.model.Project;
 import com.iae.model.Status;
 import com.iae.model.TestCase;
+
 import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
@@ -25,18 +29,26 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.control.*;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ChoiceDialog;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 import javafx.stage.Window;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
+ 
+/*  Stashed merge errors were present here for some reason -- needs testing! 
+*/
 /**
  * Controls the main application window:
  * menu bar, sidebar project tree, project-detail pane, and Run button.
